c1e8fd72e23d1ad771c1c88c536e572484ba19ef
virajdesai085 <95028288+virajdesai085@users.noreply.github.com>
2022-03-25 16:18:26 +0530

    Add files via upload
    Adding Custom models

---
+++ Custom.cs
@@ -0,0 +1,31 @@
+﻿using Helperland_projectContext.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Helperland.Models
+{
+    public class Custom
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ServiceRequestId { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public string spFirstName { get; set; }
+        public string spLastName { get; set; }
+        public decimal Ratings { get; set; }
+        public DateTime ServiceStartDate { get; set; } = DateTime.Now;
+        public int? Status { get; set; }
+        public int? ServiceProviderId { get; set; }
+        public decimal TotalCost { get; set; }
+
+    }
+}
---
+++ SPnewServiceRequests.cs
@@ -0,0 +1,51 @@
+﻿using Helperland_projectContext.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Models
+{
+    public class SPnewServiceRequests
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ServiceRequestId { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+
+        public int ServiceId { get; set; }
+        public DateTime ServiceStartDate { get; set; } = DateTime.MinValue;
+        public string ZipCode { get; set; }
+        public decimal? ServiceHourlyRate { get; set; }
+        public double ServiceHours { get; set; }
+        public double? ExtraHours { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal TotalCost { get; set; }
+        public string Comments { get; set; }
+        public string PaymentTransactionRefNo { get; set; }
+        public bool PaymentDue { get; set; }
+        public int? ServiceProviderId { get; set; }
+        public DateTime? SpacceptedDate { get; set; } = DateTime.UtcNow;
+        public bool HasPets { get; set; }
+        public int? Status { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+        public int? ModifiedBy { get; set; }
+        public decimal? RefundedAmount { get; set; }
+        public decimal Distance { get; set; }
+        public bool? HasIssue { get; set; }
+        public bool? PaymentDone { get; set; }
+        public Guid? RecordVersion { get; set; }
+
+        
+    }
+}
