70411fda1beac9b483265d44080ceb399c1eca84
virajdesai085 <95028288+virajdesai085@users.noreply.github.com>
2022-03-25 16:17:03 +0530

    Add files via upload
    Add Custom models

---
+++ Helperland/Models/UserManagement.cs
@@ -0,0 +1,23 @@
+﻿using Helperland_projectContext.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Models
+{
+    public class UserManagement
+    {
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string ZipCode { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public int UserTypeId { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
