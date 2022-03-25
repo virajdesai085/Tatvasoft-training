aafaa923b302cca702b34eb2a9d42acdd79a4a57
virajdesai085 <95028288+virajdesai085@users.noreply.github.com>
2022-03-25 16:21:58 +0530

    Update User.cs
    Validation changes

--- Helperland/Models/User.cs
+++ Helperland/Models/User.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
-namespace Helperland.Models
+namespace Helperland_projectContext.Models
 {
     public partial class User
     {
@@ -18,11 +20,32 @@ namespace Helperland.Models
             UserAddresses = new HashSet<UserAddress>();
         }
 
+
+
         public int UserId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide First Name")]
         public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide First Name")]
         public string LastName { get; set; }
+        [Required(AllowEmptyStrings = false ,ErrorMessage = "Please enter your email address")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
         public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Please Enter Password." )]
+        [StringLength(30,MinimumLength = 2, ErrorMessage ="password must contain minimum 8 letters.")]
         public string Password { get; set; }
+     
+        [NotMapped, DataType(DataType.Password)]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Password and Confirm Password must match.")]
+        public string ConfirmPassword { get; set; }
+
+        [NotMapped, DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Contact is required")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10,MinimumLength =10)]
         public string Mobile { get; set; }
         public int UserTypeId { get; set; }
         public int? Gender { get; set; }
@@ -30,6 +53,7 @@ namespace Helperland.Models
         public string UserProfilePicture { get; set; }
         public bool IsRegisteredUser { get; set; }
         public string PaymentGatewayUserRef { get; set; }
+        [StringLength(6, MinimumLength = 4, ErrorMessage = "Required Field!")]
         public string ZipCode { get; set; }
         public bool WorksWithPets { get; set; }
         public int? LanguageId { get; set; }
@@ -43,6 +67,7 @@ namespace Helperland.Models
         public int? Status { get; set; }
         public string BankTokenId { get; set; }
         public string TaxNo { get; set; }
+        
 
         public virtual ICollection<FavoriteAndBlocked> FavoriteAndBlockedTargetUsers { get; set; }
         public virtual ICollection<FavoriteAndBlocked> FavoriteAndBlockedUsers { get; set; }
