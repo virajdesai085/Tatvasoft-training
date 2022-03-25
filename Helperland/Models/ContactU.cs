2b4f79c878ce988d235f907c31cf8db3eec6a147
virajdesai085 <95028288+virajdesai085@users.noreply.github.com>
2022-03-25 16:22:30 +0530

    Update ContactU.cs
    Validation changes

--- Helperland/Models/ContactU.cs
+++ Helperland/Models/ContactU.cs
@@ -1,20 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
-namespace Helperland.Models
+namespace Helperland_projectContext.Models
 {
     public partial class ContactU
     {
         public int ContactUsId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Name")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Name Should be min 2 and max 20 length")]
         public string Name { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter your email address")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
         public string Email { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Subject { get; set; }
+        [Required(ErrorMessage = "Contact is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Please enter valid Mobile number")]
         public string PhoneNumber { get; set; }
+        [StringLength(200)]
+        [Required]
         public string Message { get; set; }
+        [StringLength(50)]
         public string UploadFileName { get; set; }
-        public DateTime? CreatedOn { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedOn { get; set; }
         public int? CreatedBy { get; set; }
         public string FileName { get; set; }
     }
